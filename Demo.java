8344c6722ab5b15428464f952cab730a3653543e
Julio <jpaucarc@unsa.edu.pe>
2023-05-07 21:09:54 -0500

    Implementación Laboratorio 01

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+  public static void main(String[] args) {
+    CubbyHole c = new CubbyHole();
+    Productor p1 = new Productor(c, 1);
+    Consumidor c1 = new Consumidor(c, 1);
+    p1.start();
+    c1.start();
+  }
+}
